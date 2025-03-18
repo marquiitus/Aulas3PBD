86ac7960c19ececc66da0b43ace5dc417361870a
Marcos Souza <152660845+marquiitus@users.noreply.github.com>
2025-03-18 10:31:02 -0300

    Update faculdade.sql

--- exercicios/exercicio1/faculdade.sql
+++ exercicios/exercicio1/faculdade.sql
@@ -2,7 +2,7 @@ CREATE DATABASE faculdade;  /*criando o banco de dados*/
 USE faculdade; 
 
 CREATE TABLE IF NOT EXISTS aluno (  /*criando as tabelas*/
-	id_aluno int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,  
+	id_aluno int(10) PRIMARY KEY AUTO_INCREMENT,  
   matricula int(10) NOT NULL UNIQUE,
   email varchar(50) NOT NULL UNIQUE,
   nome varchar(50) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS aluno (  /*criando as tabelas*/
 );
 
 CREATE TABLE IF NOT EXISTS disciplina (
-	id_disciplina int(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id_disciplina int(5) PRIMARY KEY AUTO_INCREMENT,
   sigla varchar(6) NOT NULL UNIQUE,
   nome varchar(60) NOT NULL,
   carga_horaria int NOT NULL,
