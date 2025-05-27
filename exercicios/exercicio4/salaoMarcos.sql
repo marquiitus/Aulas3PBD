652c60ea716fcade9a458c3a4d49b37070ef306c
Marcos Souza <marcosfdesouza13@gmail.com>
2025-05-27 20:51:14 -0300

    commit intermediário

--- exercicios/exercicio4/salaoMarcos.sql
+++ exercicios/exercicio4/salaoMarcos.sql
@@ -1,7 +1,4 @@
-CREATE DATABASE salaoMarcos;
-USE salaoMarcos;
-
-CREATE TABLE IF NOT EXISTS Cliente (
+CREATE TABLE IF NOT EXISTS Cliente(
 	id_cliente int(10) PRIMARY KEY AUTOINCREMENT,
   nome varchar(20) not NULL,
   telefone varchar(15) NOT NULL UNIQUE,
@@ -10,9 +7,17 @@ CREATE TABLE IF NOT EXISTS Cliente (
   saldo_credito double(10) 
 ); 
 
-CREATE TABLE IF NOT EXISTS Pacote (
+CREATE TABLE IF NOT EXISTS FilaEspera(
+	id_fila int(10) PRIMARY KEY AUTOINCREMENT,
+  hora_entrada varchar(20) NOT NULL,
+  status varchar(20) NOT NULL,
+  FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+);
+
+CREATE TABLE IF NOT EXISTS Pacote(
 	id_pacote int(10) PRIMARY KEY AUTOINCREMENT,
   nome_pacote varchar(15) NOT NULL,
   descricao varchar(40) NOT NULL,
   preco_total double(10) NOT NULL
 );
+
